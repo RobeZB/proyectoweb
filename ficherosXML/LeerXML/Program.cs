73fee4e11e9e4d6fe932687a566b7f1262887cba
RobeZB <74201052+RobeZB@users.noreply.github.com>
2021-04-25 00:47:40 +0200

    adsa

---
+++ ficherosXML/LeerXML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+namespace LeerXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("D:\\Roberto\\Documents\\1DAW\\PaginaWeb\\publico\\XML\\fichero.xml"); 
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                String id = node.Attributes["id"].Value;
+                Console.WriteLine("id: " + id);
+                if (node.HasChildNodes)
+                {
+                    for (int i = 0; i < node.ChildNodes.Count; i++)
+                    {
+                        Console.WriteLine(node.ChildNodes[i].Name + " : " + node.ChildNodes[i].InnerText);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
